b3fc5a73e816ab9511262acaac8c647a7336d941
Alex Grönholm <alex.gronholm@nextday.fi>
2019-12-25 12:38:37 +0200

    Fixed wrong exception name in __all__

--- sniffio/__init__.py
+++ sniffio/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for sniffio."""
 
+__all__ = [
+    "current_async_library", "AsyncLibraryNotFoundError",
+    "current_async_library_cvar"
+]
+
 from ._version import __version__
 
 from ._impl import (
@@ -7,8 +12,3 @@ from ._impl import (
     AsyncLibraryNotFoundError,
     current_async_library_cvar,
 )
-
-__all__ = [
-    "current_async_library", "UnknownAsyncLibraryError",
-    "current_async_library_cvar"
-]
